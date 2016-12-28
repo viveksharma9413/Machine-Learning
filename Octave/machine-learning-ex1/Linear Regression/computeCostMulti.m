5c3ab9a01f8980697f6c90e1bb65b2ae2a751b88
Vivek <vivek@particle41.com>
2016-12-28 13:03:04 +0530

    Completed code for Gradient Descent and Cost Function for Multivariate Linear Regression in octave

--- Octave/machine-learning-ex1/Linear Regression/computeCostMulti.m
+++ Octave/machine-learning-ex1/Linear Regression/computeCostMulti.m
@@ -12,7 +12,10 @@ J = 0;
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
+hypothesis = X * theta;
+sqrErrs = (hypothesis - y) .^ 2;
 
+J = 1/(2*m) * sum(sqrErrs)
 
 
 
