3d82706263ec711c582661e3885140f26fbc1db6
Vivek <vivek@particle41.com>
2016-12-28 11:27:54 +0530

    Completed code for gradientDescent for univarialte Linear Regression

--- Octave/machine-learning-ex1/Linear Regression/gradientDescent.m
+++ Octave/machine-learning-ex1/Linear Regression/gradientDescent.m
@@ -6,7 +6,6 @@ function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 % Initialize some useful values
 m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
-
 for iter = 1:num_iters
 
     % ====================== YOUR CODE HERE ======================
@@ -16,7 +15,7 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
+    theta = theta - (X' * (X * theta - y) .* (alpha/m));
 
 
 
