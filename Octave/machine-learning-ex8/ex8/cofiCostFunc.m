82bed4a47092405b86bf3c5c6782b690b466f306
Vivek Sharma <vivek@particle41.com>
2017-03-07 09:34:05 +0530

    Added code to find cost function for collaborative filtering. Finished the assignment for week 9 Anomaly Detection and Recommender systems

--- Octave/machine-learning-ex8/ex8/cofiCostFunc.m
+++ Octave/machine-learning-ex8/ex8/cofiCostFunc.m
@@ -40,20 +40,12 @@ Theta_grad = zeros(size(Theta));
 %                     partial derivatives w.r.t. to each element of Theta
 %
 
+temp = (X * Theta' - Y).^2;
 
+J = sum((temp(R == 1))(:))/2 + lambda/2 .* sum((Theta.^2)(:)) + lambda/2 .* sum((X.^2)(:));
 
-
-
-
-
-
-
-
-
-
-
-
-
+X_grad = ((X * Theta' - Y) .* R) * Theta + lambda.*X;
+Theta_grad = ((X * Theta' - Y) .* R)' * X + lambda.*Theta;
 
 % =============================================================
 
