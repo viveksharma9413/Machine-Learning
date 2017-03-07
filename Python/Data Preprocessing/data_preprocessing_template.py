c654ed4e530e9a566b4fbaef55253893f8f032e8
Vivek Sharma <vivek@particle41.com>
2017-03-07 23:20:41 +0530

    Added implementation of taking care of missing data in the dataset and encoding of categorical data

--- Python/Data Preprocessing/data_preprocessing_template.py
+++ Python/Data Preprocessing/data_preprocessing_template.py
@@ -1,7 +1,27 @@
 # -*- coding: utf-8 -*-
-
+#importing libraries
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+np.set_printoptions(threshold=np.inf)
+
+#importing dataset
 dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:,:-1].values
+y = dataset.iloc[:,-1].values
+
+#handling missing values
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values='NaN',strategy='mean',axis=0)
+imputer.fit(X[:,1:])
+X[:,1:] = imputer.transform(X[:,1:])
+
+#encoding categorical data
+from sklearn.preprocessing import LabelEncoder,OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:,0] = labelencoder_X.fit_transform(X[:,0])
+onehotencoder = OneHotEncoder(categorical_features = [0])
+X = onehotencoder.fit_transform(X).toarray()
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
