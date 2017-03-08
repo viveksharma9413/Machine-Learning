1b7ac5fa5ffeec268972c24f6ba1c3e1ce3c9473
Vivek Sharma <vivek@particle41.com>
2017-03-08 13:39:14 +0530

    Replaced the deprecated cross_validation class with model_selection class

--- Python/Simple_Linear_Regression/salary_prediction_model.py
+++ Python/Simple_Linear_Regression/salary_prediction_model.py
@@ -8,7 +8,7 @@ dataset = pd.read_csv('Salary_Data.csv')
 X = dataset.iloc[:,:-1].values
 y = dataset.iloc[:,-1].values
 
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 X_train,X_test,y_train,y_test = train_test_split(X,y,train_size=2/3,random_state=0)
 
 #Applying Simple Linear Regression
