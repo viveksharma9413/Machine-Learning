1fed246d733baf2e4def8cc77f03a353b9f088f2
Vivek Sharma <vivek@particle41.com>
2017-03-08 10:52:15 +0530

    Added implementation of feature scaling

--- Python/Data Preprocessing/data_preprocessing_template.py
+++ Python/Data Preprocessing/data_preprocessing_template.py
@@ -29,3 +29,9 @@ y = labelencoder_y.fit_transform(y)
 #splitting the data set into training and test set
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X,y,train_size=0.8, random_state=0)
+
+#feature scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.fit_transform(X_test)
