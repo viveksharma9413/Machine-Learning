1298179b839f38858f0f9962791853db065d02db
Vivek Sharma <vivek@particle41.com>
2017-03-08 12:44:12 +0530

    Added the implementation of applying simple linear regression to training set

--- Python/Simple_Linear_Regression/salary_prediction_model.py
+++ Python/Simple_Linear_Regression/salary_prediction_model.py
@@ -2,9 +2,16 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+#data preprocessing step
+
 dataset = pd.read_csv('Salary_Data.csv')
 X = dataset.iloc[:,:-1].values
 y = dataset.iloc[:,-1].values
 
 from sklearn.cross_validation import train_test_split
 X_train,X_test,y_train,y_test = train_test_split(X,y,train_size=2/3,random_state=0)
+
+#Applying Simple Linear Regression
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()              #Machine
+regressor.fit(X_train,y_train)              #Making the machine to learn
