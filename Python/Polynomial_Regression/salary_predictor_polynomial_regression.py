02e8f2a94d00ff99be140262b2a7e39616c0dd27
Vivek Sharma <vivek@particle41.com>
2017-03-10 11:49:34 +0530

    smoothed the graphical visualization of the polynomial regression by using numpy library

--- Python/Polynomial_Regression/salary_predictor_polynomial_regression.py
+++ Python/Polynomial_Regression/salary_predictor_polynomial_regression.py
@@ -6,7 +6,8 @@ dataset = pd.read_csv('Position_Salaries.csv')
 
 X = dataset.iloc[:,1:2].values
 y = dataset.iloc[:,2].values
-
+X_grid = np.arange(min(X),max(X),0.1)
+X_grid = X_grid.reshape(len(X_grid),1)
 # Fitting linear regression to the dataset
 from sklearn.linear_model import LinearRegression
 linear_reg = LinearRegression()
@@ -14,11 +15,13 @@ linear_reg.fit(X,y)
 
 # visualization of Linear Regression
 plt.scatter(X,y,color='cyan')
-plt.plot(X,linear_reg.predict(X),color='red')
+plt.plot(X_grid,linear_reg.predict(X_grid),color='red')
 plt.title('Truth or Bluff (Linear Regression)')
 plt.xlabel('Job Levels')
 plt.ylabel('Salary')
 plt.show()
+plt.savefig('Linear Regression smooth graph.png');
+
 
 # Fitting polynomial regression to the dataset
 """Converting X feature matrix to polynomial feature matrix"""
@@ -32,12 +35,12 @@ for deg in range(2,5):
 
     #visualization of Polynomial Regression
     plt.scatter(X,y,color='cyan')
-    plt.plot(X,lin_reg2.predict(X_poly),color='red')
+    plt.plot(X_grid,lin_reg2.predict(poly_reg.fit_transform(X_grid)),color='red')
     plt.title('Truth or Bluff (Polynomial Regression)')
     plt.xlabel('Job Levels')
     plt.ylabel('Salary')
     fig = plt.figure(1)
-    fig.canvas.set_window_title('Polynomial Regression degree '+str(deg))
+    fig.canvas.set_window_title('Polynomial Regression smooth graph with degree '+str(deg))
     plt.show()
-    plt.savefig('Polynomial Regression degree '+str(deg)+'.png');
+    plt.savefig('Polynomial Regression smooth graph with degree '+str(deg)+'.png');
 
